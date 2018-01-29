bea95d59d58efa627ff92ce726522c775f1cb824
ostway <ostway@gmail.com>
2018-01-30 01:02:11 +0300

    1

--- scr/ImageHasher.cs
+++ scr/ImageHasher.cs
@@ -3,10 +3,9 @@ using System.Drawing;
 
 namespace ImageHasher
 {
-	// Token: 0x02000003 RID: 3
 	public static class ImageHasher
 	{
-		// Token: 0x06000004 RID: 4 RVA: 0x000021C4 File Offset: 0x000003C4
+
 		static ImageHasher()
 		{
 			for (int i = 0; i < 256; i++)
@@ -24,13 +23,11 @@ namespace ImageHasher
 			}
 		}
 
-		// Token: 0x06000005 RID: 5 RVA: 0x00002244 File Offset: 0x00000444
 		private static int CountSetBits(int n)
 		{
 			return ImageHasher.CountSetBits((ulong)((long)n));
 		}
 
-		// Token: 0x06000006 RID: 6 RVA: 0x00002250 File Offset: 0x00000450
 		private static int CountSetBits(ulong n)
 		{
 			int num = 0;
@@ -45,20 +42,18 @@ namespace ImageHasher
 			return num;
 		}
 
-		// Token: 0x06000007 RID: 7 RVA: 0x00002278 File Offset: 0x00000478
+
 		public static int ComputeHammingDistance(ulong n1, ulong n2)
 		{
 			ulong num = n1 ^ n2;
 			return (int)(ImageHasher.BitCounts16[(int)(checked((IntPtr)((num & 18446462598732840960UL) >> 48)))] + ImageHasher.BitCounts16[(int)(checked((IntPtr)((num & 281470681743360UL) >> 32)))] + ImageHasher.BitCounts16[(int)(checked((IntPtr)((num & unchecked((ulong)-65536)) >> 16)))] + ImageHasher.BitCounts16[(int)(checked((IntPtr)(num & 65535UL)))]);
 		}
 
-		// Token: 0x06000008 RID: 8 RVA: 0x000022DB File Offset: 0x000004DB
 		public static double ComputeSimilarity(ulong hash1, ulong hash2)
 		{
 			return (double)(64 - ImageHasher.ComputeHammingDistance(hash1, hash2)) * 0.015625;
 		}
 
-		// Token: 0x06000009 RID: 9 RVA: 0x000022F4 File Offset: 0x000004F4
 		public static ulong ComputeAverageHash(Image image)
 		{
 			ulong num = 0UL;
@@ -85,7 +80,6 @@ namespace ImageHasher
 			return num;
 		}
 
-		// Token: 0x0600000A RID: 10 RVA: 0x0000238C File Offset: 0x0000058C
 		public static ulong ComputeDifferenceHash(Image image)
 		{
 			ulong num = 0UL;
@@ -111,10 +105,8 @@ namespace ImageHasher
 			return num;
 		}
 
-		// Token: 0x04000001 RID: 1
 		private static byte[] BitCounts8 = new byte[256];
 
-		// Token: 0x04000002 RID: 2
 		private static ushort[] BitCounts16 = new ushort[65536];
 	}
 }
--- scr/ImageUtils.cs
+++ scr/ImageUtils.cs
@@ -5,10 +5,8 @@ using System.Drawing.Imaging;
 
 namespace ImageHasher
 {
-	// Token: 0x02000002 RID: 2
 	public static class ImageUtils
 	{
-		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
 		public static Bitmap ReduceImage(Image image, int width, int height)
 		{
 			Bitmap bitmap = new Bitmap(width, height, PixelFormat.Format24bppRgb);
@@ -23,7 +21,6 @@ namespace ImageHasher
 			return bitmap;
 		}
 
-		// Token: 0x06000002 RID: 2 RVA: 0x000020B8 File Offset: 0x000002B8
 		public static Bitmap GrayscaleImage(Bitmap bmp)
 		{
 			Bitmap bitmap = new Bitmap(bmp.Width, bmp.Height, PixelFormat.Format24bppRgb);
@@ -42,7 +39,6 @@ namespace ImageHasher
 			return bitmap;
 		}
 
-		// Token: 0x06000003 RID: 3 RVA: 0x00002130 File Offset: 0x00000330
 		public static byte[] GetGrayscaleBytes(Bitmap bmp)
 		{
 			byte[] array = new byte[bmp.Height * bmp.Width];
